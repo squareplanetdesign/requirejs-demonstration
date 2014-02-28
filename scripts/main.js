e67dd8d3f06df75e941f30b9db6b9ee1c3ef72c0
Tom Green <tomgreen981111@yahoo.com>
2014-02-28 14:36:36 -0600

    Step 3: Added index and main script.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
-
+require([], function() {
+    window.setTimeout(function() {
+        var node = document.getElementById("node");
+        if (node) {
+            node.innerHTML = "The farm is run by the animals!!!";
+        }
+    }, 1000);
+});
