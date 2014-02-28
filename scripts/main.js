a92b92ec718135ea5068a1de5bb3341763caeb6e
Tom Green <tomgreen981111@yahoo.com>
2014-02-28 17:22:57 -0600

    Step 8: Optimization...

--- scripts/main.js
+++ scripts/main.js
@@ -7,24 +7,27 @@ require.config({
 
 require(["jquery", "pig", "horse", "text!template.html"], function($, Pig, Horse, html) {
     var node = $("#node").parent();
+    var id = 0;
+
+    /**
+     * Add a node to the dom
+     */
+    var addNode = function(node, text) {
+        var newNode = $($.parseHTML(html)[0]);
+        newNode.attr("id", "message" + id++);
+        newNode.text(text);
+        node.append(newNode);
+    };
+
     window.setTimeout(function() {
         if (node) {
-            var newNode = $($.parseHTML(html)[0]);
-            newNode.attr("id", "message1");
-            newNode.text("The farm is run by the animals!!!");
-            node.append(newNode);
+            addNode(node, "The farm is run by the animals!!!");
             var pig = new Pig("Napoleon");
             var horse = new Horse("Boxer");
             window.setTimeout(function() {
-                var newNode = $($.parseHTML(html)[0]);
-                newNode.attr("id", "message1");
-                newNode.text(pig.says());
-                node.append(newNode);
+                addNode(node, pig.says());
                 window.setTimeout(function() {
-                    var newNode = $($.parseHTML(html)[0]);
-                    newNode.attr("id", "message1");
-                    newNode.text(horse.says());
-                    node.append(newNode);
+                    addNode(node, horse.says());
                 }, 1000);
             }, 1000);
         }
