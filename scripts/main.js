4c2536389068b09d0812dac9706dfc6d6e3a7325
Tom Green <tomgreen981111@yahoo.com>
2014-02-28 16:25:26 -0600

    Step 6: Changing technology to use non-amd library, jquery.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,21 @@
-require(["pig", "horse"], function(Pig, Horse) {
-    var node = document.getElementById("node");
+require.config({
+    baseUrl: "scripts",
+    paths: {
+        jquery: "jquery-2.1.0.min"
+    }
+});
+
+require(["jquery", "pig", "horse"], function($, Pig, Horse) {
+    var node = $("#node");
     window.setTimeout(function() {
         if (node) {
-            node.innerHTML = "The farm is run by the animals!!!";
+            node.text("The farm is run by the animals!!!");
             var pig = new Pig("Napoleon");
             var horse = new Horse("Boxer");
             window.setTimeout(function() {
-                node.innerHTML = pig.says();
+                node.text(pig.says());
                 window.setTimeout(function() {
-                    node.innerHTML = horse.says();
+                    node.text(horse.says());
                 }, 1000);
             }, 1000);
         }
